b87e2ed6e0d041d112e2653ed99cbd3dd6335c0f
Darah Via <ddmoscoso@up.edu.ph>
2023-04-10 21:11:27 +0800

    Lecture 4 Assignment

--- Lecture4/Assignments/as4.c
+++ Lecture4/Assignments/as4.c
@@ -3,10 +3,15 @@ int main(void){
     long long int n, power2, c;
     printf("enter n: ");        //prompt the user to input n
     scanf("%lld", &n);        // read input and store to variable n
-    power2 = 1;
-    for (c = 0; c < n; c++){
+
+    printf("TABLE OF POWERS OF TWO\n");
+    printf(" n   2 to the n\n");
+    printf("--- -------------\n");
+    // loop n times to calculate the power of 2]
+    power2 = 1;     
+    for (c = 0; c <= n; c++){
+        printf(" %-9d%lld\n", c, power2);     //print the result 
         power2 *= 2;
-        }
-    printf("%d = %lld", n, power2);
+    }
 }
 
--- Lecture4/Assignments/as5.c
+++ Lecture4/Assignments/as5.c
@@ -4,24 +4,27 @@
 
 int main(void){
     int numDays, startDay, days, count;
-    do{    
-        printf("Enter number of days in month: ");
-        scanf("%d", &numDays);
-        printf("Enter the starting day of the week (1 = Sun, 7 = Sat):");
-        scanf("%d", &startDay);   
-    }while(numDays < 28 || numDays > 31 || startDay < 1 || startDay > 7);
-  
-    for (count = 1; count < startDay; count++){
-        printf("   ");              // print spaces before beginning of day
-    }
-    for (days = 1; days <= numDays; days++, count++){
-        if (days < 10){
-            printf(" %d ", days);   // print spaces to align single-digit days to double-digit days
-        }else{
-            printf("%d ", days);}   // prit double-digit days
-        if (count % MAX_DAY == 0){            // change row if reached 7th column
-            printf("\n");
+    printf("Enter number of days in month: ");
+    scanf("%d", &numDays);
+    printf("Enter the starting day of the week (1 = Sun, 7 = Sat):");
+    scanf("%d", &startDay); 
+
+    if(numDays < 28 || numDays > 31 || startDay < 1 || startDay > 7){
+        printf("invalid input");
+
+    }else{
+        for (count = 1; count < startDay; count++){
+            printf("   ");              // print spaces before beginning of day
         }
+        for (days = 1; days <= numDays; days++, count++){
+            if (days < 10){
+                printf(" %d ", days);   // print spaces to align single-digit days to double-digit days
+            }else{
+                printf("%d ", days);}   // print double-digit days
+            if (count % MAX_DAY == 0){            // change row if reached 7th column
+                printf("\n");
+            }
+        }        
     }
 }
 
