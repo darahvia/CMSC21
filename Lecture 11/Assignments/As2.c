95319f746ed968dd9e601acfc33189226b144841
Darah Via <ddmoscoso@up.edu.ph>
2023-06-01 07:03:29 +0800

    Lecture 11 Assignment

---
+++ Lecture 11/Assignments/As2.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <stdbool.h> 
+#define SIZE 26
+
+void scan_word(int *occurences);
+bool is_anagram(int *occurences1, int *occurences2);
+
+int main(void){
+
+    int i,
+        occurences1[SIZE] = {0},
+        occurences2[SIZE] = {0};
+
+    printf("Enter first word: ");
+    scan_word(occurences1);
+    printf("Enter second word: ");
+    scan_word(occurences2);
+    if (is_anagram(occurences1, occurences2)){
+        printf("The words are anagrams.\n");
+    }
+    else{
+        printf("The words are not anagrams.\n");
+    }
+}
+
+void scan_word(int *occurences){
+    char c;
+    while ((c = getchar()) != '\n'){
+        if (isalpha(c)){
+            occurences[toupper(c) - 'A']++;  
+        }
+    }
+}
+
+bool is_anagram (int *occurences1, int *occurences2){
+    int i, j;
+    for (i = 0; i < 26; i++) {
+        if (occurences1[i] != occurences2[i]) {
+            return false;
+        }
+    }
+    return true;
+}
